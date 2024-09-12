50f3ee125381f8fcfa1e048d158212b75ad2d490
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2024-09-12 17:04:12 -0600

    Start searching through knightTravails array using BFS

--- knightTravails.js
+++ knightTravails.js
@@ -34,4 +34,38 @@ for (let x = 0; x < knightTravails.length; x++) {
   }
 }
 
-console.log(knightTravails[3][3]);
+function knightMoves(src, dst) {
+  let f = src[0],
+    l = src[1],
+    q = [],
+    visited = Array(8).fill(false),
+    count = 0;
+
+  visited[src] = true;
+  src = knightTravails[f][l];
+  q.push(src);
+  console.log(dst);
+  while (q.length) {
+    if (q[0].length === 0) {
+      q.shift();
+    }
+    const curr = q[0].shift();
+    console.log("Queue:", q);
+
+    if (!visited[curr] && curr !== undefined) {
+      visited[curr] = true;
+      q.push(knightTravails[curr[0]][curr[1]]);
+      count++;
+    }
+    // for (const x of knightTravails[curr[0]][curr[1]]) {
+
+    // }
+    if (curr === dst) {
+      return count;
+    }
+  }
+}
+
+// console.log(knightTravails[2][1]);
+// console.log(knightTravails[7][7]);
+console.log(knightMoves([0, 0], [7, 7]));
